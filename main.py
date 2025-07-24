db618c472e93b01517498534327e82741e23176a
Aayush <162538524+ZenSlashBS@users.noreply.github.com>
2025-07-24 20:39:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 # main.py
 import os
 import threading
-from bot import run_bot
+from bot import bot
 from app import app
 from db import init_db
 
 init_db()
 
-bot_thread = threading.Thread(target=run_bot, daemon=True)
-bot_thread.start()
-
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
+    if 'RAILWAY_PUBLIC_DOMAIN' in os.environ:
+        app_url = f"https://{os.environ['RAILWAY_PUBLIC_DOMAIN']}"
+        bot.remove_webhook()
+        bot.set_webhook(url=app_url + '/webhook')
+    else:
+        # Local development: use polling
+        threading.Thread(target=bot.infinity_polling, daemon=True).start()
+
     app.run(host='0.0.0.0', port=port, debug=False)
